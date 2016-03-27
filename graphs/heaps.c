74d069017a0baeece6904d17b25ea3630ddff273
Reid Paape <wrpaape@gmail.com>
2016-03-27 10:19:02 -0500

    testing insert function on binary heap

--- graphs/heaps.c
+++ graphs/heaps.c
@@ -1,35 +1,75 @@
-#include <graphs.h>
-#include <heaps.h>
+#include "graphs.h"
+#include "heaps.h"
+#include "limits.h"
 
-struct BinaryHeap *init_binary_heap(int root_value, int (*compare)(int, int))
+
+struct BinaryHeap *init_binary_heap(int (*compare)(int, int))
 {
 	struct BinaryHeap *heap;
-	struct HeapNode *root;
 
 	HANDLE_MALLOC(heap, sizeof(struct BinaryHeap));
-	HANDLE_MALLOC(root, sizeof(struct HeapNode));
+	HANDLE_MALLOC(heap->nodes, sizeof(int) * 2);
 
-	root->value  = root_value;
-	root->parent = NULL;
-	root->lchild = NULL;
-	root->rchild = NULL;
-
-	heap->compare = compare;
-	heap->root    = root;
-}
-
-void binary_heap_insert_array(struct BinaryHeap *heap, int length, int *array)
-{
-
-
-
-
-	for (int i = 0; i < length; ++i) {
-		binary_heap_insert()
-	}
+	/* set first node as sentinel, will never move */
+	heap->nodes[0]	  = compare(INT_MIN, INT_MAX) ? INT_MIN : INT_MAX;
+	heap->nodes_count = 1lu;
+	heap->alloc_count = 2lu;
 
 	return heap;
 }
 
 
+
+struct BinaryHeap *array_into_binary_heap(int length,
+					  int *array,
+					  int (*compare)(int, int))
+{
+	struct BinaryHeap *heap = init_binary_heap(compare);
+
+	binary_heap_insert_array(heap, length, array);
+
+	return heap;
+}
+
+void binary_heap_insert_array(struct BinaryHeap *heap, int length, int *array)
+{
+	for (int i = 0; i < length; ++i)
+		binary_heap_insert_value(heap, array[i]);
+}
+
+
 void binary_heap_insert_value(struct BinaryHeap *heap, int value)
+{
+	insert(heap->nodes, heap->nodes_count, value, heap->compare);
+
+	if (heap->nodes_count == heap->alloc_count)
+		resize_binary_heap(heap, heap->alloc_count * 2);
+
+}
+
+void insert(int *nodes, int base_i, int value, int (*compare)(int, int))
+{
+	int par_i  = base_i / 2;
+	int parent = nodes[par_i];
+
+	if (compare(parent, value)) {
+
+		nodes[base_i] = value;
+		return;
+	}
+
+	nodes[base_i] = parent;
+
+	insert(nodes, par_i, value, compare);
+}
+
+inline void resize_binary_heap(struct BinaryHeap *heap, size_t size)
+{
+	if (realloc(heap->nodes, sizeof(int) * size) == NULL) {
+		EXIT_ON_FAILURE("failed to reallocate number of words"
+				"from %lu to %lu",
+				heap->alloc_count, size);
+	}
+
+	heap->alloc_count = size;
+}
--- graphs/heaps.h
+++ graphs/heaps.h
@@ -1,16 +1,22 @@
-struct HeapNode {
-	int value;
-	struct HeapNode *parent;
-	struct HeapNode *lchild;
-	struct HeapNode *rchild;
-};
-
 struct BinaryHeap {
 	int (*compare)(int, int);
-	struct HeapNode *root;
-	struct HeapNode *base;
+	int *nodes;
+	size_t nodes_count;
+	size_t alloc_count;
 };
 
-struct BinaryHeap *init_binary_heap(int root_value, int (*compare)(int, int));
+struct BinaryHeap *init_binary_heap(int (*compare)(int, int));
+
+struct BinaryHeap *array_into_binary_heap(int length,
+					  int *array,
+					  int (*compare)(int, int));
+
 void binary_heap_insert_array(struct BinaryHeap *heap, int length, int *array);
+
 void binary_heap_insert_value(struct BinaryHeap *heap, int value);
+
+static void insert(int *nodes, int par_i, int value, int (*compare)(int, int));
+
+static inline void resize_binary_heap(struct BinaryHeap *heap, size_t size);
+
+
