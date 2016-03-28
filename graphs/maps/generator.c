3fa1498bc761333f0671911278ced33a017a7024
Reid Paape <wrpaape@gmail.com>
2016-03-28 18:38:11 -0500

    hooked up psg basic

--- graphs/maps/generator.c
+++ graphs/maps/generator.c
@@ -1,8 +1,8 @@
 #include "utils.h"
 #include "maps/generator.h"
 #include <time.h>
-#include <pcg_basic.h>
 
+#define MAX_RAND (1u << 32)
 
 int **generate_map(const size_t res_x,
 		   const size_t res_y,
@@ -12,22 +12,22 @@ int **generate_map(const size_t res_x,
 
 	int **grid;
 
-	srand(time(NULL));
+	pcg32_random_t rng;
+	pcg32_srandom_r(&rng, time(NULL), (intptr_t)&rng);
 
-	int cost = rand_in_range(min_cost, max_cost);
+	int cost = rand_in_range(&rng, min_cost, max_cost);
 
 	return NULL;
 }
 
-inline int rand_in_range(const int lbound,
+inline int rand_in_range(pcg32_random_t *rngptr,
+			 const int lbound,
 			 const int rbound)
 {
 
-	pcg32_random_t rng1, rng2, rng3;
-	pcg32_srandom_r(&rng1, time(NULL), (intptr_t)&rng1);
-	pcg32_srandom_r(&rng2, time(NULL), (intptr_t)&rng2);
-	pcg32_srandom_r(&rng3, time(NULL), (intptr_t)&rng3);
 
-	return rand_val;
+	printf("rng: %u\n", (unsigned) pcg32_random_r(rngptr));
+
+	return 0;
 }
 
