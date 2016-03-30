0c16594c5b39ce84fe30b74a132b703677eebbcb
Reid Paape <wrpaape@gmail.com>
2016-03-30 10:33:56 -0500

    new coord system for goal and start

--- graphs/maps/printer.c
+++ graphs/maps/printer.c
@@ -14,29 +14,6 @@ do {									\
 				FILENAME, MODE);			\
 } while (0)
 
-void (*COST_TOKEN_SETTERS[SETTER_COUNT])(char **) = {
-	set_cost_token_0, set_cost_token_1, set_cost_token_2, set_cost_token_3,
-	set_cost_token_4, set_cost_token_5, set_cost_token_6, set_cost_token_7,
-	set_cost_token_8
-};
-
-struct JoinFuns TOP_LINE_JOIN_FUNS = {
-	.left   = set_top_left_join;
-	.center = set_top_center_join;
-	.right  = set_top_right_join;
-}
-
-struct JoinFuns MID_LINE_JOIN_FUNS = {
-	.left   = set_top_left_join;
-	.center = set_top_center_join;
-	.right  = set_top_right_join;
-}
-
-struct JoinFuns BOT_LINE_JOIN_FUNS = {
-	.left   = set_top_left_join;
-	.center = set_top_center_join;
-	.right  = set_top_right_join;
-}
 
 
 void pretty_print_cost_map(char *buffer,
@@ -71,16 +48,13 @@ void pretty_print_cost_map(char *buffer,
 
 	char *buff_ptr;
 
-	struct Lines *lines = draw_lines(res_x);
-	char *mid_line      = lines->mid;
-
-
 	int *costs_row;
 	size_t x, y;
 
 
 
 	x = 0lu;
+
 
 	while (1) {
 
@@ -157,83 +131,4 @@ void cost_map_to_csv(char *filename,
 
 
 	fclose(map_file);
-}
-
-
-struct Lines *draw_lines(const size_t res_x)
-{
-	/* 'res_x * 4': length of  '───' plus joiner */
-	/* '_____ + 1': plus left piece */
-	/* '_____ * 3': 3 bytes per box drawing character */
-	/* '_____ + 2': newline plus null byte */
-	const size_t BUFF_CHARS = (((res_x * 4lu) + 1lu) * 3lu) + 2lu;
-
-	const size_t size = sizeof(char) * BUFF_CHARS;
-
-	struct Lines *lines;
-	char *top;
-	char *mid;
-	char *bot;
-	size_t cell_i;
-	size_t char_i;
-
-	HANDLE_MALLOC(lines, sizeof(struct Lines));
-	HANDLE_MALLOC(top,   size);
-	HANDLE_MALLOC(mid,   size);
-	HANDLE_MALLOC(bot,   size);
-
-	lines->top = top;
-	lines->mid = mid;
-	lines->bot = bot;
-	lines->size = size;
-
-	/* set white_bg ANSI */
-
-	/* set left pieces '┌', '├', and '└' */
-	PUT_BOX_CHAR_LIGHT_NW_CORNER(top);
-	PUT_BOX_CHAR_LIGHT_W_JOIN(mid);
-	PUT_BOX_CHAR_LIGHT_SW_CORNER(bot);
-
-	cell_i = 1lu;
-
-	while (1) {
-
-		/* set 3 horizontal lines '─' (top/bottom borders of cells) */
-		for (char_i = 0lu; char_i < 3lu; ++char_i) {
-			PUT_BOX_CHAR_LIGHT_H_LINE(top);
-			PUT_BOX_CHAR_LIGHT_H_LINE(mid);
-			PUT_BOX_CHAR_LIGHT_H_LINE(bot);
-		}
-
-		/* if on last cell... */
-		if (cell_i == res_x)
-			break; /* break, terminate lines and return */
-
-		/* otherwise set joiner pieces '┬', '┼', and '┴' */
-		PUT_BOX_CHAR_LIGHT_N_JOIN(top);
-		PUT_BOX_CHAR_LIGHT_CROSS(mid);
-		PUT_BOX_CHAR_LIGHT_S_JOIN(bot);
-
-		++cell_i;
-	}
-
-	/* set right pieces '┐', '┤', and '┘' */
-	PUT_BOX_CHAR_LIGHT_NE_CORNER(top);
-	PUT_BOX_CHAR_LIGHT_E_JOIN(mid);
-	PUT_BOX_CHAR_LIGHT_SE_CORNER(bot);
-
-	/* terminate with newlines and null byte */
-	PUT_CHAR(top, '\n'); PUT_CHAR(top, '\0');
-	PUT_CHAR(mid, '\n'); PUT_CHAR(mid, '\0');
-	PUT_CHAR(bot, '\n'); PUT_CHAR(bot, '\0');
-
-	return lines;
-}
-
-inline void free_lines(struct Lines *lines)
-{
-	free(lines->top);
-	free(lines->mid);
-	free(lines->bot);
-	free(lines);
 }
