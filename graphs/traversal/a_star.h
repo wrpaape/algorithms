601125c46546948ef4373900c147054e347f7f3e
Reid Paape <wrpaape@gmail.com>
2016-03-31 12:00:14 -0500

    getting rid of set_weights function

--- graphs/traversal/a_star.h
+++ graphs/traversal/a_star.h
@@ -3,9 +3,8 @@
 
 struct AStarWeights {
 	const int min_cost;
-	const int cost_range;
-	const size_t ini_prox;
-	const size_t prox_range;
+	const double w_cost;
+	const double w_prox;
 };
 
 struct AStarStepNode {
@@ -23,12 +22,13 @@ struct AStarPathNode {
 
 struct AStarAcc {
 	int cost;
-	size_t steps;
+	size_t steps_taken;
 	struct AStarPathNode *last;
 	struct AStarPathNode *head;
 };
 
 struct AStarResults {
+	size_t least_steps;
 	size_t count_explored;
 	clock_t time_elapsed;
 	struct AStarAcc *best;
@@ -36,8 +36,16 @@ struct AStarResults {
 
 struct AStarResults *a_star_least_cost_path(struct CostMap *map);
 
+void set_a_star_weights(struct AStarWeights *WEIGHTS,
+			const int min_cost,  const int max_cost
+			const size_t max_x,  const size_t max_y,
+			const size_t goal_x, const size_t goal_y);
+
 void report_a_star_results(struct AStarResults *results);
 void a_star_step_node_to_string(char *buffer, const void *vstep);
+
+static inline void calc_prox(const size_t x0, const size_t y0,
+			     const size_t x1, const size_t y1);
 
 inline void free a_star_results(struct AStarResults *results)
 {
