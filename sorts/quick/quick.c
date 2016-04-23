26c7db586a9ba8a8a0b17c287d3e1d7193a62f3d
Reid Paape <wrpaape@gmail.com>
2016-04-23 09:45:40 -0500

    working on split

--- sorts/quick/quick.c
+++ sorts/quick/quick.c
@@ -12,86 +12,8 @@ do {				\
 	++PTR;			\
 } while (0)
 
-static inline bool compare(int x,
-			  int y)
-{
-	return x < y;
-}
-
-static inline void swap(int *data,
-			const size_t i,
-			const size_t j)
-{
-	const int tmp = data[i];
-	data[i] = data[j];
-	data[j] = tmp;
-}
-
-
-int split_data(int *data,
-               size_t i,
-               size_t j)
-{
-	int split = data[i];
-
-	while (1) {
-		while (compare(split, data[j])) {
-			--j;
-			if (i == j)
-				return i + 1ul;
-		}
-
-		++i;
-		if (i == j)
-			return i - 1ul;
-
-		while (compare(data[i], split)) {
-			++i;
-			if (i == j)
-				return i - 1ul;
-		}
-
-
-		swap(data, i, j);
-
-
-		--j;
-		if (i == j)
-			return i + 1ul;
-	}
-}
-
-void do_sort(int *data,
-	     const size_t i_first,
-	     const size_t i_last)
-{
-	if (i_first < i_last) {
-
-		if (i_first + 1ul == i_last) {
-
-			if (compare(data[i_last], data[i_first]))
-				swap(data, i_last, i_first);
-
-			return;
-		}
-		const size_t i_split = split_data(data, i_first, i_last);
-
-		do_sort(data, i_first,	     i_split);
-		do_sort(data, i_split + 1ul, i_last);
-	}
-}
-
-
-
-static inline void quick_sort(int *data,
-			      const size_t length)
-{
-	do_sort(data, 0ul, length - 1ul);
-}
-
 static inline size_t num_digits(const int n)
 {
-
 	if (n < 100000) {
 		if (n < 1000) {
 			if (n < 100)
@@ -150,18 +72,126 @@ static inline void put_data(char *ptr,
 	PUT_CHAR(ptr, '}'); PUT_CHAR(ptr, '\0');
 }
 
+static inline bool compare(int x,
+			   int y)
+{
+	return x < y;
+}
+
+static inline void swap(int *data,
+			const size_t i,
+			const size_t j)
+{
+	const int tmp = data[i];
+	data[i] = data[j];
+	data[j] = tmp;
+}
+
+
+size_t split_data(int *data,
+               size_t i,
+               size_t k)
+{
+	const size_t length = k - i + 1ul;
+	const size_t i_pivot = i;
+	const int pivot = data[i];
+
+	size_t j = k - 1ul;
+
+	char buffer[1000];
+	put_data(buffer,
+		 &data[i_pivot],
+		 length);
+
+	printf("*******************************\ninit:\n%s\n", buffer);
+
+	while (1) {
+		++i;
+
+		while (1) {
+			if (i == j)
+				goto PLACE_PIVOT;
+
+			++i;
+
+			if (compare(pivot, data[i]))
+				break;
+		}
+
+		printf("-----\npivot: %d, data[i]: %d, i: %zu\n", pivot, data[i], i);
+
+		while (1) {
+			if (i == j)
+				goto PLACE_PIVOT;
+
+			if (compare(data[k], pivot))
+				break;
+
+			k = j;
+
+			--j;
+		}
+		printf("pivot: %d, data[k]: %d, k: %zu\n", pivot, data[k], k);
+
+		swap(data, i, k);
+	}
+
+PLACE_PIVOT:
+
+	if (compare(data[j], pivot)) {
+		--j;
+	}
+
+	swap(data, i_pivot, j);
+
+
+
+	put_data(buffer,
+		 &data[i_pivot],
+		 length);
+
+	printf("final:\n%s\ni: %zu\nj: %zu\nk: %zu\nl: %zu\np: %d\n\n", buffer, i, j, i_pivot, length, pivot);
+
+	fflush(stdout);
+
+	usleep(100000);
+
+	return j;
+}
+
+void do_sort(int *data,
+	     const size_t i_first,
+	     const size_t i_last)
+{
+	if (i_first < i_last) {
+
+		const size_t i_split = split_data(data, i_first, i_last);
+
+		do_sort(data, i_first,	     i_split - 1ul);
+		do_sort(data, i_split + 1ul, i_last);
+	}
+}
+
+
+
+static inline void quick_sort(int *data,
+			      const size_t length)
+{
+	do_sort(data, 0ul, length - 1ul);
+}
+
 
 int main(void)
 {
 	char raw[LENGTH_DATA * 12ul];
 	char sorted[LENGTH_DATA * 12ul];
-	int data[LENGTH_DATA] = {8, 2, 33, 55, 5, 1, 199, 3};
+	int data[LENGTH_DATA];
 	time_t dummy;
 
 	srand((unsigned) time(&dummy));
 
-	/* for (size_t i = 0ul; i < LENGTH_DATA; ++i) */
-	/* 	data[i] = rand(); */
+	for (size_t i = 0ul; i < LENGTH_DATA; ++i)
+		data[i] = rand() % 100;
 
 	put_data(&raw[0l], data, LENGTH_DATA);
 
@@ -170,7 +200,7 @@ int main(void)
 
 	put_data(&sorted[0l], data, LENGTH_DATA);
 
-	printf("raw:\n%s\nsorted:\n:%s\n", raw, sorted);
+	printf("raw:\n%s\nsorted:\n%s\n", raw, sorted);
 
 	return 0;
 }
