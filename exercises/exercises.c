24926dc518bb639d6ed02519b3601ca9bf69017e
Reid Paape <wrpaape@gmail.com>
2016-04-25 18:25:46 -0500

    ez pz

--- exercises/exercises.c
+++ exercises/exercises.c
@@ -2,10 +2,109 @@
 #include <stdlib.h>
 #include "exercises.h"
 
+#define GRINCH_N (1ul << 4)
+
 
 int main(void)
 {
-	init_rng();
-	printf("%s\n", coin_flip() ? "HEADS" : "TAILS");
+	run_grinch();
 	return 0;
+}
+
+inline void swap_ij(int *data,
+		    const ptrdiff_t i,
+		    const ptrdiff_t j)
+{
+	const int tmp = data[i];
+	data[i] = data[j];
+	data[j] = tmp;
+}
+
+ptrdiff_t split_data(int *data,
+		     ptrdiff_t from,
+		     ptrdiff_t upto)
+{
+	const ptrdiff_t i_pivot = from;
+	const int pivot = data[i_pivot];
+
+	while (1) {
+		while (1) {
+			++from;
+
+			if (from == upto) {
+				if (data[from] > pivot)
+					--from;
+
+				goto PLACE_PIVOT;
+			}
+
+			if (data[from] > pivot)
+				break;
+		}
+
+		while (1) {
+			if (data[upto] < pivot)
+				break;
+
+			--upto;
+
+			if (upto == from) {
+				--from;
+				goto PLACE_PIVOT;
+			}
+
+		}
+
+		swap_ij(data, from, upto);
+	}
+
+PLACE_PIVOT:
+	swap_ij(data, i_pivot, from);
+	return from;
+}
+
+inline void do_sort(int *data,
+		    const ptrdiff_t from,
+		    const ptrdiff_t upto)
+{
+	if (from < upto) {
+		const ptrdiff_t split = split_data(data, from, upto);
+		do_sort(data, from, split - 1);
+		do_sort(data, split + 1, upto);
+	}
+}
+
+
+void run_grinch(void)
+{
+	int players[GRINCH_N];
+	ptrdiff_t i;
+	const ptrdiff_t split = GRINCH_N / 2l;
+
+	init_rng();
+
+	for (i = 0; i < GRINCH_N; ++i)
+		players[i] = (int) rand_uint_upto(500u);
+
+	do_sort(players, 0l, GRINCH_N - 1l);
+
+	int good_avg = 0;
+	int bad_avg  = 0;
+
+	do {
+		--i;
+		good_avg += players[i];
+	} while (i > split);
+
+	do {
+		--i;
+		bad_avg += players[i];
+	} while (i >= 0l);
+
+	for (i = 0; i < GRINCH_N; ++i)
+		printf("players[%zd]: %d\n", i, players[i]);
+
+	printf("good: %ld\nbad:  %ld\ntot:  %ld\n",
+	       good_avg / split, bad_avg / split,
+	       (good_avg + bad_avg) / GRINCH_N);
 }
--- exercises/exercises.h
+++ exercises/exercises.h
@@ -1,2 +1,16 @@
 #include <utils/rand.h>
-#include <utils/token.h>
+#include <stddef.h>
+
+void run_grinch(void);
+
+static inline void do_sort(int *data,
+			   const ptrdiff_t from,
+			   const ptrdiff_t upto);
+
+static inline void swap_ij(int *data,
+			   const ptrdiff_t i,
+			   const ptrdiff_t j);
+
+ptrdiff_t split_data(int *data,
+		     ptrdiff_t from,
+		     ptrdiff_t upto);
