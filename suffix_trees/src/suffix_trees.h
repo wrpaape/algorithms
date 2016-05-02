f73be9e13548e82afc48ee623ce68ca0ec2528af
Reid Paape <wrpaape@gmail.com>
2016-05-02 17:36:41 -0500

    packing build state into a struct

--- suffix_trees/src/suffix_trees.h
+++ suffix_trees/src/suffix_trees.h
@@ -12,12 +12,22 @@ struct SuffixEdge {
 	struct SuffixNode *children;
 };
 
+struct SuffixBuildState {
+	struct SuffixNode *act_node;
+	struct SuffixEdge *act_edge;
+	size_t act_length;
+	size_t rem_suffixes;
+};
+
 
 char **pattern_find_all(char *const restrict string,
 			char *const restrict pattern);
 
 
 struct SuffixNode *create_suffix_tree(char *restrict string);
+
+void append_suffix(struct SuffixBuildState *state,
+		   char *const string);
 
 static inline struct SuffixNode *init_suffix_node(char *const restrict upto);
 
