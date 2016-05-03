037236bb5dc007b1527a57a5d0d2050ff719920f
Reid Paape <wrpaape@gmail.com>
2016-05-03 16:14:39 -0500

    chuggging

--- suffix_tree/src/suffix_tree.c
+++ suffix_tree/src/suffix_tree.c
@@ -41,13 +41,13 @@ struct SuffixTree *build_suffix_tree(char *string)
 	/* count of all nodes in tree */
 	const size_t tot_node_cnt = ext_node_cnt + int_node_cnt;
 
+	struct SuffixTree *tree;
 	struct SuffixNode **root_map;
-	struct SuffixNode **map_buff;
-	struct SuffixNode *int_buff;
-	struct SuffixNode *ext_buff;
+	struct SuffixNode *internal;
+	struct SuffixNode *node;
 
 	/* allocate memory for all nodes and edge_maps upfront */
-	HANDLE_MALLOC(ext_buff,
+	HANDLE_MALLOC(node,
 		      sizeof(struct SuffixNode) * tot_node_cnt);
 
 	/* all nodes are preceeded by an edge_map, init all maps to NULL */
@@ -55,34 +55,51 @@ struct SuffixTree *build_suffix_tree(char *string)
 		      CHAR_SPAN * tot_node_cnt,
 		      sizeof(struct SuffixNode *));
 
-	/* partion buffers:
-	/* - split node buffer into 'external' and 'internal' halves */
-	/* - reserve first edge_map for 'root_map' */
-	int_buff = &ext_buff[ext_node_cnt];
-	map_buff = &root_map[CHAR_SPAN];
+	/* initialize tree */
+	HANDLE_MALLOC(tree,
+		      sizeof(struct SuffixTree));
 
+	tree->root_map     = root_map;
+	tree->node_buff    = node;
+	tree->int_node_cnt = int_node_cnt;
+	tree->ext_node_cnt = ext_node_cnt;
+	tree->tot_node_cnt = tot_node_cnt;
 
 	/* initialize nodes before insertion into tree */
-	ptrdiff_t i_node;
+	internal = node;
 
-	/* leaf nodes:
-	 * - set 'base' pointers to start of suffix
-	 * - set 'edge_map' to NULL */
-	for (i_node = 0l; i_node < ext_node_cnt; ++i_node) {
-		ext_buff[i_node].base = &string[i_node];
-		ext_buff[i_node].edge_map = NULL;
-	}
+	/* if no internal nodes, skip initialization */
+	if (int_node_cnt == 0ul)
+		goto INIT_LEAF_NODES;
+
+	/* partion buffers:
+	/* - split node buffer into 'internal' and 'external' sections
+	/* - reserve first edge_map for 'root_map' */
+	struct SuffixNode *external  = internal + int_node_cnt;
+	struct SuffixNode **next_map = root_map + CHAR_SPAN;
 
 	/* internal nodes:
 	 * - set 'edge_map' to next available span of NULL node pointers */
-	for (i_node = 0l; i_node < int_node_cnt; ++i_node)
-		int_buff[i_node].edge_map = &map_buff[i_node * CHAR_SPAN];
+	while (1) {
+		node->edge_map = next_map;
+		++node;
+
+		if (node == external)
+			break;
+
+		next_map += CHAR_SPAN;
+	}
+
+INIT_LEAF_NODES:
+	/* leaf nodes:
+	 * - set 'base' pointers to start of suffix
+	 * - set 'edge_map' to NULL */
+	for (i = 0l; i < ext_node_cnt; ++i) {
+		ext_nodes[i].base = &string[i];
+		ext_nodes[i].edge_map = NULL;
 
 
-	/* build suffix tree by incremental insertion of each leaf node */
+	}
 
-
-
-
-	return NULL;
+	return tree;
 }
