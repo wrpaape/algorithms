2724136cc7b3a674b37273f12a464e69945192f9
Reid Paape <wrpaape@gmail.com>
2016-11-11 16:37:49 -0600

    gacp new array representaation of graph

--- tsp/usa/src/usa.c
+++ tsp/usa/src/usa.c
@@ -1,6 +1,5 @@
 #include "usa.h"
 
-static struct Path path_map[50];
 static unsigned int distance_map[50][50];
 static const char *location_map[50];
 char *restrict distances_buffer;
@@ -51,13 +50,12 @@ get_distance_row(unsigned int *restrict distance,
 static inline void
 init_tsp_state(void)
 {
-	unsigned int node1;
-	unsigned int node2;
+	unsigned int node;
+	unsigned int next_node;
 	unsigned int *restrict row;
 	unsigned int *restrict next_row;
-	unsigned int *restrict node1_ptr;
-	unsigned int *restrict node2_ptr;
-	unsigned int *restrict distance;
+	unsigned int *restrict node_ptr;
+	unsigned int *restrict distance_ptr;
 	const char *restrict ptr;
 	const char *restrict failure;
 	const char *restrict *restrict location;
@@ -67,19 +65,14 @@ init_tsp_state(void)
 				&failure)))
 	    exit_on_failure(failure);
 
-	ptr	  = distances_buffer;
-	location  = &location_map[0];
-	path	  = &path_map[0];
-	row	  = &distance_map[0][0];
-	next_row  = row + 50;
-	node1     = 0u;
-	node2	  = 1u;
-	node1_ptr = &solution[0];
-	distance  = node1_ptr + 1;
-	node2_ptr = node1_ptr + 2;
-
-	const unsigned int *const restrict node_until
-	= node1_ptr + 100;
+	ptr	     = distances_buffer;
+	location     = &location_map[0];
+	row	     = &distance_map[0][0];
+	next_row     = row + 50;
+	node         = 0u;
+	next_node    = 1u;
+	node_ptr     = &solution[0];
+	distance_ptr = node_ptr + 1;
 
 	do {
 		get_location(location,
@@ -89,18 +82,18 @@ init_tsp_state(void)
 				 next_row,
 				 (const char **) &ptr);
 
-		*node1_ptr = node1;
-		*distance  = row[node2];
-		*node2_ptr = node2;
-
-		total_distance += *distance;
+		*node_ptr	= node;
+		*distance_ptr	= row[next_node];
+		total_distance += *distance_ptr;
 
 		++location;
-		row = next_row;
-		next_row += 50;
-		node1 = node2;
-		++node2;
-	} while (node2 < 50);
+		node_ptr     += 2;
+		distance_ptr += 2;
+		row	      = next_row;
+		next_row     += 50;
+		node	      = next_node;
+		++next_node;
+	} while (next_node < 50);
 
 	get_location(location,
 		     &ptr);
@@ -109,11 +102,9 @@ init_tsp_state(void)
 			 next_row,
 			 (const char **) &ptr);
 
-	path->node1     = node1;
-	path->next     = 0;
-	path->distance = row[0];
-
-	total_distance += path->distance;
+	*node_ptr	= node;
+	*distance_ptr	= row[0];
+	total_distance += *distance_ptr;
 
 	if (UNLIKELY(!(   signal_report(&initial_handler,
 					SIGINT,
@@ -194,14 +185,14 @@ write_solution(void)
 {
 	const unsigned int *restrict node1_ptr;
 	const unsigned int *restrict node2_ptr;
-	const unsigned int *restrict distance;
+	const unsigned int *restrict distance_ptr;
 	char *restrict ptr;
 	const char *restrict failure;
 	static char buffer[SOLUTION_BUFFER_SIZE];
 
-	node1_ptr = &solution[0];
-	distance  = node1_ptr + 1;
-	node2_ptr = node1_ptr + 2;
+	node1_ptr    = &solution[0];
+	distance_ptr = node1_ptr + 1;
+	node2_ptr    = node1_ptr + 2;
 
 	const unsigned int *const restrict node_until
 	= node1_ptr + 100;
@@ -211,17 +202,17 @@ write_solution(void)
 	do {
 		put_path(*node1_ptr,
 			 *node2_ptr,
-			 *distance,
+			 *distance_ptr,
 			 &ptr);
 
-		distance  += 2;
+		distance_ptr  += 2;
 		node1_ptr  = node2_ptr;
 		node2_ptr += 2;
 	} while (node2_ptr < node_until);
 
 	put_path(*node1_ptr,
 		 solution[0],
-		 *distance,
+		 *distance_ptr,
 		 &ptr);
 
 	free(distances_buffer);
@@ -237,7 +228,7 @@ write_solution(void)
 
 static inline void
 sample_node_indices(unsigned int *const restrict i_node1_ptr,
-		    unsigned int *const restrict i_node2_ptr);
+		    unsigned int *const restrict i_node2_ptr)
 {
 	uint32_t i_node2_div2;
 	const uint32_t i_node1_div2 = random_uint32_upto(49u);
@@ -250,16 +241,81 @@ sample_node_indices(unsigned int *const restrict i_node1_ptr,
 	*i_node2_ptr = (unsigned int) (i_node2_div2 * 2u);
 }
 
+
 static inline int
-get_swap_set(struct SwapSet *const restrict swap_set,
+do_get_swap(unsigned int *restrict *const restrict swap_distance_ptr,
+	    unsigned int *const restrict new_distance_ptr,
+	    unsigned int *const restrict old_distance_ptr,
+	    const unsigned int neighbor_node,
+	    const unsigned int *const restrict distance_swap)
+{
+	*swap_distance_ptr = old_distance_ptr;
+
+	*new_distance_ptr  = distance_swap[neighbor_node];
+
+	return *new_distance_ptr - *old_distance_ptr;
+}
+
+static inline int
+get_swap_prev(unsigned int *restrict *const restrict swap_distance_ptr,
+	      unsigned int *const restrict new_distance_ptr,
+	      unsigned int *const restrict node_ptr,
+	      const unsigned int *const restrict distance_swap)
+{
+	return do_get_swap(swap_distance_ptr,
+			   new_distance_ptr,
+			   node_ptr - 1,
+			   node_ptr[-2],
+			   distance_swap);
+}
+
+static inline int
+get_swap_next(unsigned int *restrict *const restrict swap_distance_ptr,
+	      unsigned int *const restrict new_distance_ptr,
+	      unsigned int *const restrict node_ptr,
+	      const unsigned int *const restrict distance_swap)
+{
+	return do_get_swap(swap_distance_ptr,
+			   new_distance_ptr,
+			   node_ptr + 1,
+			   node_ptr[ 2],
+			   distance_swap);
+}
+
+static inline int
+get_swap_prev_wrap(unsigned int *restrict *const restrict swap_distance_ptr,
+		   unsigned int *const restrict new_distance_ptr,
+		   unsigned int *const restrict node_ptr,
+		   const unsigned int *const restrict distance_swap)
+{
+	return do_get_swap(swap_distance_ptr,
+			   new_distance_ptr,
+			   node_ptr + 99,
+			   node_ptr[98],
+			   distance_swap);
+}
+
+static inline int
+get_swap_next_wrap(unsigned int *restrict *const restrict swap_distance_ptr,
+		   unsigned int *const restrict new_distance_ptr,
+		   unsigned int *const restrict node_ptr,
+		   const unsigned int *const restrict distance_swap)
+{
+	return do_get_swap(swap_distance_ptr,
+			   new_distance_ptr,
+			   node_ptr + 1,
+			   node_ptr[-98],
+			   distance_swap);
+}
+
+
+static inline int
+swap_set_get(struct SwapSet *const restrict swap_set,
 	     unsigned int i_node1,
 	     unsigned int i_node2)
 {
 	unsigned int *restrict *restrict swap_distance_ptr;
-	unsigned int *restrict old_distance_ptr;
 	unsigned int *restrict new_distance_ptr;
-	unsigned int old_distance;
-	unsigned int new_distance;
 	int delta_distance;
 
 	if (i_node1 > i_node2) {
@@ -269,136 +325,164 @@ get_swap_set(struct SwapSet *const restrict swap_set,
 	}
 
 	/* node1 comes before node2 */
+	unsigned int *const restrict node1_ptr = &solution[i_node1];
+	unsigned int *const restrict node2_ptr = &solution[i_node2];
 
-	unsigned int *const restrict node1 = &solution[i_node1];
-	unsigned int *const restrict node2 = &solution[i_node2];
+	const unsigned int *const restrict distance_swap1
+	= &distance_map[*node1_ptr][0];
+	const unsigned int *const restrict distance_swap2
+	= &distance_map[*node2_ptr][0];
 
-	swap_set->node1 = node1;
-	swap_set->node2 = node2;
+	swap_set->node1_ptr = node1_ptr;
+	swap_set->node2_ptr = node2_ptr;
 
-	swap_distance_ptr = &swap_set->distances[0];
+	swap_distance_ptr = &swap_set->swap_distances[0];
 	new_distance_ptr  = &swap_set->new_distances[0];
 
 	if (i_node1 == 0) {
-		} else if (i_node2 == 98) {
-			new_distance	 = distance_map[*node2][node1[ 2]];
-			old_distance_ptr = node1 + 1;
-
-			delta_distance    += new_distance - *old_distance_ptr;
-
-			*swap_distance_ptr = old_distance_ptr;
+		if (i_node2 == 98) {
+			delta_distance	= get_swap_next(swap_distance_ptr,
+							new_distance_ptr,
+							node1_ptr,
+							distance_swap2);
 			++swap_distance_ptr;
-
-			*new_distance_ptr  = new_distance;
 			++new_distance_ptr;
 
-
-			new_distance	 = distance_map[node2[-2]][*node1];
-			old_distance_ptr = node2 - 1;
-
-			delta_distance    += new_distance - *old_distance_ptr;
-
-			*new_distance_ptr  = new_distance;
-
-			*swap_distance_ptr = old_distance_ptr;
+			delta_distance += get_swap_prev(swap_distance_ptr,
+							new_distance_ptr,
+							node2_ptr,
+							distance_swap1);
 			++swap_distance_ptr;
-
 			*swap_distance_ptr = NULL;
 
 		} else {
-			new_distance	 = distance_map[node1[98]][*node2];
-			old_distance_ptr = node1 + 99;
-
-			delta_distance    += new_distance - *old_distance_ptr;
-
-			*swap_distance_ptr = old_distance_ptr;
+			delta_distance	= get_swap_prev_wrap(swap_distance_ptr,
+							     new_distance_ptr,
+							     node1_ptr,
+							     distance_swap2);
 			++swap_distance_ptr;
-
-			*new_distance_ptr  = new_distance;
 			++new_distance_ptr;
 
-
-			new_distance	 = distance_map[*node1][node2[ 2]];
-			old_distance_ptr = node2 + 1;
-
-			delta_distance    += new_distance - *old_distance_ptr;
-
-			*new_distance_ptr  = new_distance;
-
-			*swap_distance_ptr = old_distance_ptr;
+			delta_distance += get_swap_next(swap_distance_ptr,
+							new_distance_ptr,
+							node2_ptr,
+							distance_swap1);
 			++swap_distance_ptr;
 
 			if (i_node2 == 2) {
 				*swap_distance_ptr = NULL;
-				return delta_distance;
+
+			} else {
+				++new_distance_ptr;
+
+				delta_distance += get_swap_next(swap_distance_ptr,
+								new_distance_ptr,
+								node1_ptr,
+								distance_swap2);
+				++swap_distance_ptr;
+				++new_distance_ptr;
+
+				delta_distance += get_swap_prev(swap_distance_ptr,
+								new_distance_ptr,
+								node2_ptr,
+								distance_swap1);
 			}
 		}
 	} else {
-		/*  prev1   →←   distp1   →←   node1   →←   dist1n   →←   next1
-		 *	          [X]
-		 * i_node1-2   i_node1-1      i_node1      i_node1+1   i_node1+2
-		 */
-		new_distance	   = distance_map[node1[-2]][*node2];
-		old_distance_ptr   = node1 - 1;
-
-		delta_distance	   = new_distance - *old_distance_ptr;
-
-		*new_distance_ptr  = new_distance;
+		delta_distance	= get_swap_prev(swap_distance_ptr,
+						new_distance_ptr,
+						node1_ptr,
+						distance_swap2);
+		++swap_distance_ptr;
 		++new_distance_ptr;
 
-		*swap_distance_ptr = old_distance_ptr;
-		++swap_distance_ptr;
+		if (i_node2 == 98) {
+			delta_distance += get_swap_next_wrap(swap_distance_ptr,
+							     new_distance_ptr,
+							     node2_ptr,
+							     distance_swap1);
 
+			++swap_distance_ptr;
 
-		/*  prev2   →←   distp2   →←   node2   →←   dist2n   →←   next2
-		 *	                                      [X]
-		 * i_node2-2   i_node2-1      i_node2      i_node2+1   i_node2+2
-		 */
-		new_distance	   = distance_map[*node1][node2[ 2]];
-		old_distance_ptr   = node2 + 1;
+			if (i_node1 == 96) {
+				*swap_distance_ptr = NULL;
 
-		delta_distance    += new_distance - *old_distance_ptr;
+			} else {
+				++new_distance_ptr;
 
-		*new_distance_ptr  = new_distance;
+				delta_distance += get_swap_next(swap_distance_ptr,
+								new_distance_ptr,
+								node1_ptr,
+								distance_swap2);
+				++swap_distance_ptr;
+				++new_distance_ptr;
 
-		*swap_distance_ptr = old_distance_ptr;
-		++swap_distance_ptr;
+				delta_distance += get_swap_prev(swap_distance_ptr,
+								new_distance_ptr,
+								node2_ptr,
+								distance_swap1);
+			}
+		} else {
+			delta_distance += get_swap_next(swap_distance_ptr,
+							new_distance_ptr,
+							node2_ptr,
+							distance_swap1);
 
-		if (i_node2 == (i_node1 + 2)) {
-			*swap_distance_ptr = NULL;
-			return delta_distance;
+			++swap_distance_ptr;
+
+			if (i_node2 == (i_node1 + 2)) {
+				*swap_distance_ptr = NULL;
+
+			} else {
+				++new_distance_ptr;
+
+				delta_distance += get_swap_next(swap_distance_ptr,
+								new_distance_ptr,
+								node1_ptr,
+								distance_swap2);
+				++swap_distance_ptr;
+				++new_distance_ptr;
+
+				delta_distance += get_swap_prev(swap_distance_ptr,
+								new_distance_ptr,
+								node2_ptr,
+								distance_swap1);
+			}
 		}
 	}
-	/*  prev1   →←   distp1   →←   node1   →←   dist1n   →←   next1
-	 *	                                      [X]
-	 * i_node1-2   i_node1-1      i_node1      i_node1+1   i_node1+2
-	 */
-	new_distance	   = distance_map[*node2][node1[ 2]];
-	old_distance_ptr   = node1 + 1;
-
-	delta_distance    += new_distance - *old_distance_ptr;
-
-	++new_distance_ptr;
-	*new_distance_ptr  = new_distance;
-	++new_distance_ptr;
-
-	*swap_distance_ptr = old_distance_ptr;
-	++swap_distance_ptr;
-
-	/*  prev2   →←   distp2   →←   node2   →←   dist2n   →←   next2
-	 *	          [X]
-	 * i_node2-2   i_node2-1      i_node2      i_node2+1   i_node2+2
-	 */
-	new_distance	   = distance_map[node2[-2]][*node1];
-	old_distance_ptr   = node2 - 1;
-
-	delta_distance    += new_distance - *old_distance_ptr;
-
-	*new_distance_ptr  = new_distance;
-
-	*swap_distance_ptr = old_distance_ptr;
 
 	return delta_distance;
+}
+
+static inline void
+swap_set_swap(struct SwapSet *const restrict swap_set)
+{
+	unsigned int *restrict *restrict swap_distance_ptr;
+	unsigned int *restrict new_distance_ptr;
+
+	const unsigned int node1 = *(swap_set->node1_ptr);
+	*(swap_set->node1_ptr)	 = *(swap_set->node2_ptr);
+	*(swap_set->node2_ptr)	 = node1;
+
+	swap_distance_ptr = &swap_set->swap_distances[0];
+	new_distance_ptr  = &swap_set->new_distances[0];
+
+	**swap_distance_ptr = *new_distance_ptr;
+	++swap_distance_ptr;
+	++new_distance_ptr;
+
+	**swap_distance_ptr = *new_distance_ptr;
+	++swap_distance_ptr;
+
+	if (*swap_distance_ptr != NULL) {
+		++new_distance_ptr;
+
+		**swap_distance_ptr = *new_distance_ptr;
+
+		++swap_distance_ptr;
+		++new_distance_ptr;
+		**swap_distance_ptr = *new_distance_ptr;
+	}
 }
 
 
@@ -417,14 +501,15 @@ make_inferior_transition(const int delta_distance,
 	return transition_probability > random_probability();
 }
 
+
 static inline void
 evaluate(void)
 {
-	unsigned int old_next2;
-	unsigned int old_distance1;
-	unsigned int old_distance2;
+	unsigned int i_node1;
+	unsigned int i_node2;
 	double temperature;
 	int delta_distance;
+	struct SwapSet swap_set;
 	const char *restrict failure;
 
 
@@ -436,30 +521,19 @@ evaluate(void)
 	temperature = INITIAL_TEMPERATURE;
 
 	do {
-		/* sample_paths(&path1, */
-		/* 	     &path2); */
+		sample_node_indices(&i_node1,
+				    &i_node2);
 
-		/* old_distance1 = path1->distance; */
-		/* old_distance2 = path2->distance; */
-
-		/* delta_distance = swap_paths(path1, */
-		/* 			    path2); */
-
-		/* printf("delta_distance: %d\n", */
-		/*        delta_distance); */
+		delta_distance = swap_set_get(&swap_set,
+					      i_node1,
+					      i_node2);
 
 		if (   (delta_distance < 0)
 		    || make_inferior_transition(delta_distance,
 						temperature)) {
 			total_distance += delta_distance;
 
-		} else {
-			old_next2   = path1->next;
-			path1->next = path2->next;
-			path2->next = old_next2;
-
-			path1->distance = old_distance1;
-			path2->distance = old_distance2;
+			swap_set_swap(&swap_set);
 		}
 
 		/* (void) nanosleep(&sleep_time, */
@@ -484,7 +558,7 @@ main(void)
 {
 	init_tsp_state();
 
-	evaluate();
+	/* evaluate(); */
 
 	write_solution();
 
--- tsp/usa/src/usa.h
+++ tsp/usa/src/usa.h
@@ -11,9 +11,9 @@
 /* typedefs, struct declarations
  * ────────────────────────────────────────────────────────────────────────── */
 struct SwapSet {
-	unsigned int *restrict node1;
-	unsigned int *restrict node2;
-	unsigned int *restrict distances[4];
+	unsigned int *restrict node1_ptr;
+	unsigned int *restrict node2_ptr;
+	unsigned int *restrict swap_distances[4];
 	unsigned int new_distances[4];
 };
 
@@ -58,9 +58,38 @@ sample_node_indices(unsigned int *const restrict i_node1_ptr,
 		    unsigned int *const restrict i_node2_ptr);
 
 static inline int
-get_swap_set(struct SwapSet *const restrict swap_set,
+do_get_swap(unsigned int *restrict *const restrict swap_distance_ptr,
+	    unsigned int *const restrict new_distance_ptr,
+	    unsigned int *const restrict old_distance_ptr,
+	    const unsigned int neighbor_node,
+	    const unsigned int *const restrict distance_swap);
+static inline int
+get_swap_prev(unsigned int *restrict *const restrict swap_distance_ptr,
+	      unsigned int *const restrict new_distance_ptr,
+	      unsigned int *const restrict node_ptr,
+	      const unsigned int *const restrict distance_swap);
+static inline int
+get_swap_next(unsigned int *restrict *const restrict swap_distance_ptr,
+	      unsigned int *const restrict new_distance_ptr,
+	      unsigned int *const restrict node_ptr,
+	      const unsigned int *const restrict distance_swap);
+static inline int
+get_swap_prev_wrap(unsigned int *restrict *const restrict swap_distance_ptr,
+		   unsigned int *const restrict new_distance_ptr,
+		   unsigned int *const restrict node_ptr,
+		   const unsigned int *const restrict distance_swap);
+static inline int
+get_swap_next_wrap(unsigned int *restrict *const restrict swap_distance_ptr,
+		   unsigned int *const restrict new_distance_ptr,
+		   unsigned int *const restrict node_ptr,
+		   const unsigned int *const restrict distance_swap);
+static inline int
+swap_set_get(struct SwapSet *const restrict swap_set,
 	     unsigned int i_node1,
 	     unsigned int i_node2);
+
+static inline void
+swap_set_swap(struct SwapSet *const restrict swap_set);
 
 static inline bool
 make_inferior_transition(const int delta_distance,
